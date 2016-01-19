b60dac1c34c5686c7063f76d0c12f886b35c8db6
aviflombaum <avi@flombaum.com>
2016-01-19 15:38:45 -0500

    Add cart

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,3 +1,11 @@
 class Cart
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS carts (
+        id INTEGER PRIMARY KEY,
+        customer_id TEXT
+      )
+    SQL
+    DB[:connection].execute(sql)
+  end
 end
